54ab2bf53483fbae3340b3a731ef0790e80aa032
MohamedIbrahim206 <mo2062001mo@gmail.com>
2024-02-24 21:58:26 +0200

    Deleting webApp

--- WebApp/Spring_Backend/src/main/java/com/dmrs/demo/exception/ApiException.java
+++
@@ -1,9 +0,0 @@
-package com.dmrs.demo.exception;
-
-import org.springframework.http.HttpStatus;
-import java.time.ZonedDateTime;
-
-public record ApiException(String message, HttpStatus httpStatus,
-                           ZonedDateTime timestamp) {
-
-}
