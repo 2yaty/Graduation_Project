c270b3a4f07835060a2a4a1a1363c791c4cd7118
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2024-03-11 23:59:25 +0200

    adding readme

--- WebApp/Spring_Backend/src/main/java/com/dmrs/demo/RTVehicleData/conditions/RoadRule.java
+++
@@ -1,6 +0,0 @@
-package com.dmrs.demo.RTVehicleData.conditions;
-
-public class RoadRule {
-  private RoadSign roadSign;
-  private boolean adherence;
-}
--- WebApp/Spring_Backend/src/main/java/com/dmrs/demo/RTVehicleData/conditions/Status.java
+++
@@ -1,5 +0,0 @@
-package com.dmrs.demo.RTVehicleData.conditions;
-
-public enum Status {
-    NORMAL, AGGRESSIVE,VERY_AGGRESSIVE
-}
