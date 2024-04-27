e030e8e4734cd994a6dd199d078b1293be0a8fe8
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2024-04-27 20:40:04 +0300

    feat: adding cJSON lib.

---
+++ INITs_Uitilities/cJSON/example.h
@@ -0,0 +1,36 @@
+/*
+#include "Log_Module/cJSON.h"
+
+
+int main() {
+
+    cJSON *root;
+    cJSON *fmt = NULL;
+    root = cJSON_CreateObject();
+    cJSON_AddStringToObject(root, "name", "Jack  Nimble");
+    cJSON_AddItemToObject(root, "format", fmt = cJSON_CreateObject());
+    cJSON_AddStringToObject(fmt, "type", "rect");
+    cJSON_AddNumberToObject(fmt, "width", 1920);
+    cJSON_AddNumberToObject(fmt, "height", 1080);
+    cJSON_AddFalseToObject (fmt, "interlace");
+    cJSON_AddNumberToObject(fmt, "frame rate", 24);
+
+    printf("%s",cJSON_Print(root));
+
+    return 0;
+}
+
+
+
+------ Output ------
+{
+        "name": "Jack  Nimble",
+        "format":       {
+                "type": "rect",
+                "width":        1920,
+                "height":       1080,
+                "interlace":    false,
+                "frame rate":   24
+        }
+}
+*/
