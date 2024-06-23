deab75a0b36a0ad9183951b7d065fc7befecf222
Mustafa Ali <mustafaali4813@gmail.com>
2024-06-24 02:36:57 +0300

    minor fixes

--- Application/Off_Raspberry_Application/STM_Process.py
+++ Application/Off_Raspberry_Application/STM_Process.py
@@ -76,7 +76,7 @@ def stm_process(ser, dmrs_queue,display_output_queue,speed,StartEvent, StopEvent
             
             print("the data that should be sent to the display: ",data)
             # Send the data to the STM
-            ser.write(data.encode('utf-8'))
+            ser.write(data.encode('ascii'))
     
     # Create a thread to send the data to the STM
     send_data_thread = threading.Thread(target=send_data_to_STM , args=(ser,display_output_queue))
