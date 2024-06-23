499bbda94465f49177744595baf7fde7aec51240
Mustafa Ali <mustafaali4813@gmail.com>
2024-06-24 02:28:19 +0300

    minor fixes

--- Application/Off_Raspberry_Application/main.py
+++ Application/Off_Raspberry_Application/main.py
@@ -1,3 +1,4 @@
+import json
 import multiprocessing as mp
 import signal
 
@@ -135,7 +136,7 @@ if __name__ == "__main__":
             "totalScore": shared_trip['totalScore']
         }
     calculate_overall_score(shared_trip)
-    send_shared_trip_to_theWeb(final_trip,display_output_queue)
+    send_shared_trip_to_theWeb(json.dumps(final_trip),display_output_queue)
     dmrs_proc.terminate()
     camera_proc.terminate()
     traffic_proc.terminate()
