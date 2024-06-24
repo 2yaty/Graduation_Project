6e6d9319c02cc2b2aa608465ad88928451135b38
Mustafa Ali <mustafaali4813@gmail.com>
2024-06-24 07:00:35 +0300

    minor fixes

--- Application/Off_Raspberry_Application/LDW_Model_Process.py
+++ Application/Off_Raspberry_Application/LDW_Model_Process.py
@@ -9,8 +9,8 @@ from show_var import show
 def lane_departure_process(queue,display_output_queue):
     last_data = True
     def handle_lane_output(last,data):
-        if last_data != data:
-            last_data = data
+        if last != data:
+            last = data
             if data == True:
                 print("You are in lane")
             if data == False:
