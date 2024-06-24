76a4730264fb0c022e9eb1959cf7274bd37a0ef6
Mustafa Ali <mustafaali4813@gmail.com>
2024-06-25 00:36:28 +0300

    minor fixes

--- Application/Off_Raspberry_Application/main.py
+++ Application/Off_Raspberry_Application/main.py
@@ -140,7 +140,9 @@ if __name__ == "__main__":
         }
     calculate_overall_score(shared_trip)
     print("the trip data: ", final_trip)
+    display_output_queue.put(json.dump(final_trip))
     send_shared_trip_data(final_trip)
+    time.sleep(3)
 
     dmrs_proc.kill()
     # camera_proc.kill()
