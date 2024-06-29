db4e52bb2bd4b074624c96a33e615ea3934742a0
Salem <salemalielfaidey@gmail.com>
2024-06-29 06:46:09 +0300

    minor fixes

--- Application/Off_Raspberry_Application/main.py
+++ Application/Off_Raspberry_Application/main.py
@@ -175,8 +175,8 @@ if __name__ == "__main__":
         display_output_queue.put("*C\nFailed to Send Trip Data\n*")
     time.sleep(3)
 
-    dmrs_proc.kill()
-    #stm_proc.kill()
+    # dmrs_proc.kill()
+    stm_proc.kill()
     # camera_proc.kill()
     # traffic_proc.kill()
     # lane_proc.kill()
