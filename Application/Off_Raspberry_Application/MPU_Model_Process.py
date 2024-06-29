48ba3c0fe6114521c95049397516df703a81d24c
Salem <salemalielfaidey@gmail.com>
2024-06-29 06:20:03 +0300

    trying new window size

--- Application/Off_Raspberry_Application/MPU_Model_Process.py
+++ Application/Off_Raspberry_Application/MPU_Model_Process.py
@@ -46,7 +46,7 @@ def dmrs_process(data_queue,trip_statistics):
 
 
     # Load your exported model
-    window_size = 6
+    window_size = 10
     with open(r'/home/pi/Desktop/Final_application/Graduation_Project/Application/Off_Raspberry_Application/mobile_2_model_2_with.pkl', 'rb') as file:
         model = pickle.load(file)
 
